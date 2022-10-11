12fd33c8db68c2bdd795e0300d96ad20356d1161
Example <exampledz@outlook.com>
2022-10-11 12:32:27 -0400

    adding initial files

---
+++ UnlockAllMetaProgress/TheMod.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+
+namespace UnlockAllMetaProgress
+{
+    public class TheMod : MelonMod
+    {
+        static TheMod the_mod;
+
+        void log(string message)
+        {
+            LoggerInstance.Msg(message);
+        }
+
+        public override void OnApplicationStart()
+        {
+            log("loaded");
+            the_mod = this;
+        }
+
+        [HarmonyPatch(typeof(ItemAtlas), "CreateButton")]
+        public static class UnlockAtlasItems
+        {
+            [HarmonyPrefix]
+            static void prefix(GameObject item)
+            {
+                UnityEngine.Object.FindObjectOfType<MetaProgressSaveManager>().FindNewItem(item);
+            }
+        }
+
+        [HarmonyPatch(typeof(RunTypeButton), "Start")]
+        private static class RunButtonListener
+        {
+            static MetaProgressSaveManager.RunCompleted make_run(Character.CharacterName character, string run_type)
+            {
+                MetaProgressSaveManager.RunCompleted r = new MetaProgressSaveManager.RunCompleted();
+                r.ironMan = true;
+                r.runType = run_type;
+                r.character = character;
+                return r;
+            }
+
+            static Character.CharacterName[] characters =
+            {
+                Character.CharacterName.Purse,
+                Character.CharacterName.Tote,
+                Character.CharacterName.CR8,
+                Character.CharacterName.Satchel
+            };
+
+            [HarmonyPostfix]
+            static void postfix(RunType ___runType)
+            {
+                string mode = ___runType.name;
+                bool updated_metaprogress = false;
+                MetaProgressSaveManager metaprogress = UnityEngine.Object.FindObjectOfType<MetaProgressSaveManager>();
+                foreach (Character.CharacterName character in characters)
+                {
+                    RunType run_type = new RunType();
+                    run_type.name = mode;
+                    if (!metaprogress.RunTypeCompleted(run_type, character, true))
+                    {
+                        MetaProgressSaveManager.RunCompleted run = make_run(character, mode);
+                        metaprogress.runsCompleted.Add(run);
+                        updated_metaprogress = true;
+                        the_mod.log("Added run metaprogress: " + run.runType + ", " + run.character + ", " + run.ironMan);
+                    }
+                }
+                if (updated_metaprogress)
+                {
+                    metaprogress.GetType().GetMethod("Save", BindingFlags.NonPublic | BindingFlags.Instance)
+                        .Invoke(metaprogress, new object[] { });
+                    the_mod.log("Saved metaprogress successfully");
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+### This is a mod
+
+It unlocks all metaprogress for Backpack Hero. It runs on (TODO: find the game version where this runs) as of 2022-10-11.
+
+Metaprogress includes unlocked run types (with or without the "delete save on death" feature enabled), items and enemies seen in the item atlas, and completed tutorials.
+
+All run types will be unlocked the first time you attempt to view them.
+
+The item atlas will be unlocked as you view each individual section.
+
+It doesn't do anything about completed tutorials, since those are trivial anyway.
+
+### Note
+
+This overwrites your metaprogress file. It is strongly recommended to back up your save before using this mod if you want to return to your progress later.
+
+### Alternative to installing the mod
+
+You can also replace the `bphMeta.sav` file in your save directory with a fully unlocked file. One is available here at (TODO: upload a file).
